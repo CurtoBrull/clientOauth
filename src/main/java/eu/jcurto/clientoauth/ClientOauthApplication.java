26935b8d8a9a8bccb9c34bf63452ed4c21d7fe24
CurtoBrull <curto.brull.javier@gmail.com>
2024-07-28 19:22:34 +0200

    [SACClient-001] Initial configuration

---
+++ src/main/java/eu/jcurto/clientoauth/ClientOauthApplication.java
@@ -0,0 +1,13 @@
+package eu.jcurto.clientoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/jcurto/clientoauth/ClientOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.jcurto.clientoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
